676dd13a994d20dfdc76d041da2e96ac84b9bd6b
egichuri <eric.gichuri@andela.com>
2016-10-31 21:27:25 +0300

    [#133440997] Add basic styling Add  Bootstrap 3 for styling

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,14 @@
 from django.forms import ModelForm
-from .models import *
-
-
+from crispy_forms.helper import FormHelper
+from .models import *  
+        
 class BookForm(ModelForm):
     """ Define form fields for Book books"""
+    def __init__(self, *args, **kwargs):
+        super(BookForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_class = 'form-horizontal'
+
     class Meta:
         model = Book
         fields = ['title', 'description', 'category']
@@ -11,6 +16,11 @@ class BookForm(ModelForm):
 
 class CategoryForm(ModelForm):
     """ Define form fields for the Categories"""
+
+    def __init__(self, *args, **kwargs):
+        super(CategoryForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_class = 'form-horizontal'
 
     class Meta(ModelForm):
         model = Category
