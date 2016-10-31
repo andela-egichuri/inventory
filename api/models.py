bde71f62631c63e5c82c9b7e390fe09676a533bf
egichuri <eric.gichuri@andela.com>
2016-10-31 18:27:40 +0300

    Add Book and Category models

--- api/models.py
+++ api/models.py
@@ -2,4 +2,17 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    """ Books Category"""
+    name = models.CharField(max_length=120)
+        
+class Book(models.Model):
+    """ Book Model.
+            Defines fields for the books
+    """
+    title = models.CharField(max_length=120)
+    description = models.TextField()
+    date_added = models.DateField(auto_now_add=True)
+    date_modified = models.DateField(auto_now=True)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
