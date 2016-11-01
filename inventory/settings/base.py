0b32597e21164f096c0e858fb4034bc308cddce4
egichuri <eric.gichuri@andela.com>
2016-11-01 07:20:24 +0300

    Configure nose for running tests

--- inventory/settings/base.py
+++ inventory/settings/base.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 import os
 import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
@@ -51,6 +51,13 @@ MIDDLEWARE = [
 ]
 
 
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+
+
+NOSE_ARGS = [
+    '--with-coverage',
+    '--cover-package=app'
+]
 
 ROOT_URLCONF = 'inventory.urls'
 
